eb75431ec3b6c193fad22b2c9efc69205174b204
Byamba-Erdene <sagangansagan@gmail.com>
2024-02-23 18:22:00 +0800

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,16 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { AppCacheProvider } from '@mui/material-nextjs/v13-pagesRouter';
 import Navbar from "@/components/Navbar";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppCacheProvider {...pageProps}>
       <Navbar />
   <Component  />;
+  <Analytics />
+  <SpeedInsights />
   </AppCacheProvider>
   )
 }
